db0ac1350dfdb6f76e3e870ae97f2f43c7013079
諦念 <teinen.github@gmail.com>
2018-09-26 23:07:35 +0900

    Card Game for Two

---
+++ python/CardGameForTwo.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+  N = int(input())
+  A = sorted([int(_) for _ in input().split()], reverse=True)
+  Alice = 0
+  Bob = 0
+  for i, a in enumerate(A):
+    if i % 2 == 0:
+      Alice += a
+    else:
+      Bob += a
+  print(Alice - Bob)
+
+main()
